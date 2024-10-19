c0fa8186c9bb81eb1a8c528d6fa4988a8a069ea0
Avadhi17 <avadhisharma07@gmail.com>
2024-10-19 12:16:11 +0530

    Add files via upload

---
+++ SWEETSHOP.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include<cmath>
+
+using namespace std;
+
+int main() {
+	// your code goes here
+ int n ;
+ int x ;
+ cin >> n ;
+ cin >> x;
+ int diff = n - 10*x;
+ int j ;
+ j = diff/20  ;
+ cout << j ;
+  
+  return 0 ;
+  
+}
