0be5b486423670568fca263cdcd709eaceb44400
Avadhi17 <avadhisharma07@gmail.com>
2024-10-28 12:34:04 +0530

    Add files via upload

---
+++ FOODCOST.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int X , Y ;
+  cin >> X >> Y ;
+  int cost_per_week = 6*X + Y ;
+  cout << cost_per_week<< endl;
+  
+  return 0 ;
+  
+}
