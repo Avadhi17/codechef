b3bf1683af4bbef1c77982812e57026505b2b873
Avadhi17 <avadhisharma07@gmail.com>
2025-02-05 20:16:44 +0530

    Add files via upload

---
+++ TIMA.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int x;
+	cin >> x;
+	if(x > 2024 && x < 2051){
+	    cout << "yes" << endl;
+	}
+	else{
+	    cout << "no" << endl;
+	}
+
+}
